4107e9ce475bb0f2f581f229b6e912b6518a0b41
Taewan Park <swimtw@naver.com>
2017-11-02 16:32:18 +0900

    Fixed Gallery Refresh :)
    사진을 저장하고 갤러리를 새로고침해주지 않아 저장한 사진을 한동안 못보는 오류를 수정하였다.

--- photoeditorsdk/src/main/java/com/ahmedadeltito/photoeditorsdk/PhotoEditorSDK.java
+++ photoeditorsdk/src/main/java/com/ahmedadeltito/photoeditorsdk/PhotoEditorSDK.java
@@ -3,6 +3,8 @@ package com.ahmedadeltito.photoeditorsdk;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Typeface;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Environment;
 import android.support.annotation.ColorInt;
 import android.util.Log;
@@ -210,6 +212,14 @@ public class PhotoEditorSDK implements MultiTouchListener.OnMultiTouchListener {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+            MediaScannerConnection.scanFile(context,
+                    new String[]{file.toString()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                            Log.i("ExternalStorage", "Scanned " + path + ":");
+                            Log.i("ExternalStorage", "-> uri=" + uri);
+                        }
+                    });
         }
         return selectedOutputPath;
     }
