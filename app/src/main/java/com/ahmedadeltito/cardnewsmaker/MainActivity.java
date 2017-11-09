78ed10a63dcdabc5d65d4c598b07ab2296d7770b
Taewan Park <swimtw@naver.com>
2017-11-09 13:50:59 +0900

    Added License activity at main screen
    앱내에 라이선스 명시해주는 페이지가 따로 없어서 생성해주었다.

--- app/src/main/java/com/ahmedadeltito/cardnewsmaker/MainActivity.java
+++ app/src/main/java/com/ahmedadeltito/cardnewsmaker/MainActivity.java
@@ -3,14 +3,13 @@ package com.ahmedadeltito.cardnewsmaker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.yellowten.cardnewsmaker.LicenseActivity;
 
 public class MainActivity extends MediaActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
     public void openUserGallery(View view) {
         openGallery();
@@ -19,6 +18,28 @@ public class MainActivity extends MediaActivity {
     public void openUserCamera(View view) {
         startCameraActivity();
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 액티비티 전환
+        Button button1 = (Button)findViewById(R.id.licensecbtn);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "라이선스 명시", Toast.LENGTH_LONG).show();
+
+                // 액티비티 전환 코드
+                Intent intent = new Intent(getApplicationContext(), LicenseActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 
     @Override
     protected void onPhotoTaken() {
